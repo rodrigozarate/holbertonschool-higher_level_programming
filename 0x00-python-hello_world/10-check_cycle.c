4be6c92e18388807b85e75e63ac95cbee7ba09bd
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-06 17:33:28 -0500

    [CREATE]

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+/*
+* 10-check_cycle.c - Check for cicles in linked lists
+* Author: Rodrigo Zarate algecira
+* Date: september 06, 2021
+*/
+
+/**
+* check_cycle - Look for cilces in linjed lists
+* @list: pointer to list
+* Return: 0 if no clycle is found otherwise 1
+*/
+
+
+
+int check_cycle(listint_t *list)
+{
+	if (cicle)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
