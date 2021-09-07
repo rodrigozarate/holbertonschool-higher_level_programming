82f1e5c49fd08a6f1adfa8095fe33b8f77be361a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-07 14:51:15 -0500

    [CREATE]

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,21 @@
+/*
+* 13-insert_number.c - Insert node in sorted list
+* Author: Rodrigo Zarate Algecira
+* Date: September 7, 2021
+*/
+
+/**
+* insert_node - Insert node in sorted list
+* @head: List
+* @number: data to insert
+* Return: NULL if fail address if it works
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	if (fail)
+	{
+		return (NULL);
+	}
+return (address);
+}
