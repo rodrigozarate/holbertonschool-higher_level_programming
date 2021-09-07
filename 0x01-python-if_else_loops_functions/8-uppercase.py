5e8d3cafd0ef1e1ebcf34e034e95a4a2d8a19c44
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-07 15:59:53 -0500

    [FIX] Remove upper, change approach

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(str.upper())
-    print("")
+
+    modified_str = ""
+    for i in range(0, len(str)):
+        ascii = ord(str[i])
+
+        if (ascii >= ord('a') and
+            ascii <= ord('z')):
+            modified_str += chr(ascii - 32)
+
+        else:
+            modified_str += str[i]
+
+    print(modified_str)
