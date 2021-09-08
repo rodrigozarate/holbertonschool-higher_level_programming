a1640afb717106a9ffefbdcea5d4c4297714c7c0
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-08 16:27:55 -0500

    [FIX] Remove sorted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    unordered_names = []
-    for names in dir(hidden_4):
-        if names[0] != "_":
-            unordered_names.append(names)
+import hidden_4
 
-    ordered_names = sorted(unordered_names)
-    for reorder in ordered_names:
-        print("{}".format(reorder))
+if __name__ == "__main__":
+
+    for names in dir(hidden_4):
+        if names[0] != '_':
+            print("{}".format(names))
