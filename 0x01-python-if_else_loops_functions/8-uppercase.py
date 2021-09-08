791ce6dbcb574aaa0ca1877696976aae8aca710d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-07 21:18:31 -0500

    [FIX] change type

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,12 +3,12 @@ def uppercase(str):
 
     modified_str = ""
     for i in str:
-        ascii = ord(str[i])
+        ascii = ord(i)
 
         if (ascii >= ord('a') and
            ascii <= ord('z')):
             modified_str += chr(ascii - 32)
         else:
-            modified_str += str[i]
+            modified_str += i
 
     print(modified_str)
