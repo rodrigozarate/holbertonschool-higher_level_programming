9321e23dce574dcc85e2a2d7681d1327d11aba0b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-08 15:18:49 -0500

    [FIX] Add sort

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
+    unordered_names = []
     for names in dir(hidden_4):
-        if names[0] != "_" and names[1] != "_":
-            print("{:s}".format(names))
+        if names[0] != "_":
+            unordered_names.append(names)
+
+    ordered_names = sorted(unordered_names)
+    for reorder in ordered_names:
+        print(reorder)
