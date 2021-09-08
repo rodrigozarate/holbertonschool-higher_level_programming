68f4bd589b929a4afc3dceba986f6c3dd8751023
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-08 15:01:37 -0500

    [FIX]x

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for names in dir(hidden_4):
-        if names[0] != "_":
+        if names[0] != "_" and names[1] != "_":
             print("{:s}".format(names))
