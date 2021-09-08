5e9ead4d55a304c50a6e391e45fb0b05cd95a01b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-07 22:56:07 -0500

    [FIX] Remove doulbe line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,7 @@ def uppercase(str):
     for i in str:
         ascii = ord(i)
 
-        if (ascii >= ord('a') and
-           ascii <= ord('z')):
+        if (ascii >= ord('a') and ascii <= ord('z')):
             print("{:c}".format(ascii - 32), end="")
         else:
             print("{:s}".format(i), end="")
