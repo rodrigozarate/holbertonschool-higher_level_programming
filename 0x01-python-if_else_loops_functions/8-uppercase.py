216f200df580553c1b9468ec8d892daf5a8044bf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-07 22:31:50 -0500

    [FIX] Resolve Format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
 
-    modified_str = ""
     for i in str:
         ascii = ord(i)
 
         if (ascii >= ord('a') and
            ascii <= ord('z')):
-            modified_str += chr(ascii - 32)
+            print("{:c}".format(ascii - 32), end="")
         else:
-            modified_str += i
-
-    print(modified_str, end="")
+            print("{:s}".format(i), end="")
+    print("")
