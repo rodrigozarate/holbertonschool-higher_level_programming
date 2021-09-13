9dc825277efaf35559db22372144f337b7d15a9d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 16:12:16 -0500

    [UPDATE]

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-    cutted = ''
+        cutted = ''
         for i in my_string:
             if i == 'c' or i == 'C':
                 i = ''
-                cutted += i
+            cutted += i
     return(cutted)
