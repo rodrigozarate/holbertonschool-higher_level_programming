da2ec533b5dd04b714b2910d7b8082e3e0f723a9
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 18:41:39 -0500

    [UPDATE]

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    howlong = len(sentence)
+    if howlong > 1:
+        first = sentence[0]
+    else:
+        first = None
+
+    return(tuple(howlong, first))
