09f39539dc68f8062109e6d2da4cafb9036ec2ad
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 18:51:50 -0500

    [UPDATE]

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     howlong = len(sentence)
-    if howlong > 1:
+    if howlong > 0:
         return(howlong, sentence[0])
     else:
         return(howlong, None)
