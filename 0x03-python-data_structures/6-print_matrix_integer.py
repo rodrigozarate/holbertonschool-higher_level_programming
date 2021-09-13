4223cdcd097dc19d554012e247bf5951fc74c559
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 16:33:14 -0500

    [CREATE]

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print("{} ".format(element), end ="")
+        print("")
+
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer() 
