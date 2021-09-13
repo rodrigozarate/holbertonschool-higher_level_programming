9d4597c2c005d43a1568318af8ace1388c689267
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 15:55:27 -0500

    [UPDATE]

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,9 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
 
     if idx < 0:
-        return (new_list)
-    if idx > len(my_list):
-        return (new_list)
+        return(new_list)
+    if idx > len(my_list) - 1:
+        return(new_list)
 
     new_list[idx] = element
-    return (new_list)
+    return(new_list)
