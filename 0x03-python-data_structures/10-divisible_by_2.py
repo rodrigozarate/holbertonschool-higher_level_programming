ecac97a4f30531a1b2fe8169a89dc45edf00f2f2
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 20:05:46 -0500

    [UPDATE]

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newlist = []
+    i = 0
+    while i < len(my_list):
+        if i % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+        i += 1
+
+    return newlist
