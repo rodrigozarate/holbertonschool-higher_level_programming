59e8866182dda671302fa21e39337588a22cbdc8
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 19:16:22 -0500

    [UPDATE]

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list: return None
-    maxint = sorted(my_list, reverse = True)
+    if not my_list:
+        return None
+    maxint = sorted(my_list, reverse=True)
     return(maxint[0])
-
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
