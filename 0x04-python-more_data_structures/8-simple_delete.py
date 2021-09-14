c8bdc7e04b026e3e6d752e6957ad90de7aa3fbb3
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-14 13:32:28 -0500

    [UPDATE]

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None)
+    if key in a_dictionary:
+        a_dictionary.pop(key)
+        return a_dictionary
+    else:
+        return a_dictionary
