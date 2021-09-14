0ec9e1279d1f617b43dae4edcefbda6966b917cf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-13 20:13:51 -0500

    [UPDATE]

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
         newlist = []
         i = 0
-        while i < len(my_list) - 1:
+        while i < len(my_list):
             if my_list[i] % 2 == 0:
                 newlist.append(True)
             else:
