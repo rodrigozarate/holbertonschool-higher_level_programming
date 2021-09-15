9e0f0e9352394e9acb94477fa8c7793e0a7102a6
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-14 22:36:07 -0500

    [UPDATE]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,16 +15,59 @@ def roman_to_int(roman_string):
     romans_dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 
     sum = 0
-    
-    for a in range(len(roman_string)):
-        
-        sum += romans_dic[roman_string[a]]
+    howmany = len(roman_string)
 
-    for key in romans_dic:
-        print("{}: {}".format(key, romans_dic[key]))
+    if howmany == 0:
+        # no string
+        return 0
+    if howmany == 1:
+        # do simple replace
+        if i in romans_dic:
+            print(romans_dic[i])
+            return romans_dic[i]
+        else:
+            # malformed number
+            return 0
+
+    if howmany == 2:
+        # check for valid pairs
+
+        # sum
+        if roman_string[0] in romans_dic:
+            a = romans_dic[roman_string[0]]
+        else:
+            return 0
+            # malformed number
+
+        if roman_string[1] in romans_dic:
+            b = romans_dic[roman_string[1]]
+        else:
+            return 0
+            # malformed number
+
+        # both numbers exists at this point
+        if a >= b:
+            sum = a + b
+            print(sum)
+        else:
+            # check for malformed pairs
+            # substract
+            sum = b - a
+            print(sum)
+
+    if howmany > 2:
+        # check that no more than 3 repetitions occur
+        print(howmany)
+
+        for a in range(len(roman_string)):
+        # multiple checks
+            sum += romans_dic[roman_string[a]]
+
+        for key in romans_dic:
+            print("{}: {}".format(key, romans_dic[key]))
     
     print(sum)
     return sum
 
-i = 'LXXIV'
+i = 'LL'
 roman_to_int(i) 
