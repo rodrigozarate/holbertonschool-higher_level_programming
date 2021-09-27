c463493ad544805ac02017e52bfda3a1fea8d22d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-27 10:04:50 -0500

    [UPDATE]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-try: / except:
+    elementsPrinted = 0
+    for i in range(0, x)
+        try:
+            print(my_list=[i], end="")
+            elementsPrinted += 1
+        except:
+            break
+        print("")
+        return(elementsPrinted)
