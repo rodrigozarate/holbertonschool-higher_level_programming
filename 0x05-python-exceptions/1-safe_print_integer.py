4d2cbcef8182ad8f2efd18519bc8141d69a42d7e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-27 09:56:35 -0500

    [UPDATE]

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-try: / except:
-"{:d}".format()
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except:
+        return(False)
