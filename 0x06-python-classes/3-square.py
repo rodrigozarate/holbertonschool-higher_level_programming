d4afb55248420f7b7e2c8faf4e21016842fa62ca
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-28 16:19:27 -0500

    [FIX]

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-    """ Create a Square """
+""" Module Square """
+
+
+
 class Square:
     """ Define a square by size """
+
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be an integer")
+            raise ValueError("size must be > 0")
         else:
             self.__size = size
 
