ead6a67e2f34bea8237e8a07829dfb5b6834837b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-28 15:03:21 -0500

    [CREATE]

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Create a Square """
+class Square:
+    """ Define a square by size """
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            for i in range(self.size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2 or type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
