38731e311f95bffffba0f81f932508c0d739d17c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-28 14:19:38 -0500

    [CREATE]

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """ Create a Square "
+class Square:
+    """ Define a square by size "
+    def __init__(self, size=0):
+        if type(size) is not int:
+        elif size < 0:
+        else:
+
+    def area(self):
+        return self.__size ** 2
