7604f2cb993e26609c43e7cac80a50bde76ee850
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-29 13:12:56 -0500

    [CREATE]

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module Rectangle """
+
+
+class Rectangle:
+    """ Rectangle Class """
+    pass
+
+@attribute
+width:
+
+@property 
+def width(self):
+TypeError
+height must be an integer
+ValueError
+height must be >= 0
+
+def width(self, value):
+
+@attribute
+height:
+
+@property
+def height(self):
+TypeError
+height must be an integer
+ValueError
+height must be >= 0
+
+def height(self, value):
+
+def __init__(self, width=0, height=0):
+
+def area(self):
+
+def perimeter(self):
+if width == 0 or height == 0:
+perimeter is equal to 0
