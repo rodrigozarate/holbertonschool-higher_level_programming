c248516dae8c07799a34327b3609d4102c911d59
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-30 00:01:30 -0500

    [TEST]

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -32,11 +32,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    def area(self):
-        return self.__width * self.__height
-
-    def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return(self.__width * 2) + (self.__height * 2)
