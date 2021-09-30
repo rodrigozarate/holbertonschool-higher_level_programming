3f0c97a3efb31ad2427af6fe3d5c7adb70452cde
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-29 23:13:45 -0500

    [FIX]

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""Class Rectangle not empty"""
+""" Class Rectangle not empty """
 
 
 class Rectangle:
-    """Rectangle Class empty"""
-    dict
+    """ Rectangle Class empty """
 
     def __init__(self, width=0, height=0):
-        """Something goes here"""
         self.height = height
         self.width = width
 
     @property
     def height(self):
-        """Something goes here"""
         return self._height
 
     @height.setter
     def height(self, value):
-        """Something goes here"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -27,12 +23,10 @@ class Rectangle:
 
     @property
     def width(self):
-        """Something goes here"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Something goes here"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
