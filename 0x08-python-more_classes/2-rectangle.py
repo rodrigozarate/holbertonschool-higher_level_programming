2af5fd0742c42327c5012c572a2555ec0b7f7c5b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-29 23:47:13 -0500

    [UPDATE]

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,74 +3,35 @@
 
 
 class Rectangle:
-    """
-        Rectangle Class
+    """ class Rectangle """
 
-        Attribute:
-            width (int): Private
-            height (int): Private
-
-    """
     def __init__(self, width=0, height=0):
-        """
-            Init Rectangle Class
-
-            Args:
-                width (int): width
-                height (int): height
-        """
         self.height = height
         self.width = width
 
-    @property 
-    def width(self):
-        """
-           width method get
-
-           Return: width (int)
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """
-           width method set
-
-        Args:
-            Value (int): value
-        Raises:
-            TypeError: value not (int)
-            ValueError: value < 0
-        """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     @property
     def height(self):
-        """
-           height method get
-        Return: height (int)
-        """
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """
-           height method set
-        Args:
-            Value (int): value
-        Raises:
-            TypeError: value not (int)
-            ValueError: value < 0
-        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property 
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     def area(self):
         return self.__width * self.__height
