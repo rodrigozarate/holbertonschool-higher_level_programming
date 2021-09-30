8ec72f1b4182d5bc337831f50fba8e0c02f0ae0d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-29 23:50:18 -0500

    [FIX]

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Module Rectangle """
+""" Module Rectangle not empty """
 
 
 class Rectangle:
-    """ class Rectangle """
+    """ class Rectangle empty """
 
     def __init__(self, width=0, height=0):
         self.height = height
