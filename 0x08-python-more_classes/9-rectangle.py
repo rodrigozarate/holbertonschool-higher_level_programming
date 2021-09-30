428ee47b999ac5ff955bca589dac49d265e584fb
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-30 18:39:10 -0500

    [UPDATE]

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -66,7 +66,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return Rectangle(size, size)
  
     def __del__(self):
         type(self).number_of_instances -= 1
