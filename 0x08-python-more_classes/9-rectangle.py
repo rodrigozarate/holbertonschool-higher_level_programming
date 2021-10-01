1ae02fcc3ca8f09ac1792c884ec1745825c45587
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-09-30 20:02:24 -0500

    [FIX]

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def height(self):
@@ -70,5 +70,4 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """ square Rectangle """
         return cls(size, size)
