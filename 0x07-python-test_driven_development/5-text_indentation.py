d5ad978941e3b412cb90b6b4bf04c4b8569dabd3
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-06 22:38:50 -0500

    [UPDATE]

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
+"""
+Module identation by text
+"""
+
+
 def text_indentation(text):
+    """
+    text with new lines
+    """
+    if not isinstance(text, str):
+        TypeError("text must be a string")
+
+    lentext = 0
+    while text[lentext] == ' ' and lentext < len(text):
+        lentext += 1
+
+    while lentext < len(text):
+        print(text[lentext], end="")
+        if text[lentext] == "\n" or text[lentext] in ".:?":
+            if text[lentext] in ".:?"
+                print("\n")
+            lentext += 1
+            while lentext < len(text) and text[lentext] == ' ':
+                 lentext += 1
+            continue
+        lentext += 1
