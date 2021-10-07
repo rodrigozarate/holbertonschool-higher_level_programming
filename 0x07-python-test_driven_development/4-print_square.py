83fbe02fad987533518766848a6bf33a855bf49d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-06 22:09:28 -0500

    [UPDATE]

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""
+Module to print square
+"""
+
+
 def print_square(size):
+    """
+    Print a square based on the size
+    """
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * size)
