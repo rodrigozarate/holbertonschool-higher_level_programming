251cb0bedbc650abb5b7ba801879bd364f0bd2d8
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 12:54:37 -0500

    [CREATE]

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module definition of student class """
+
+
+class Student():
+    """ Class to define a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
