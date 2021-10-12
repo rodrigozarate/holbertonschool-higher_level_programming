34e887f7f37535a76156667dd855582f9b22aed7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 13:16:23 -0500

    [CREATE]

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module definition of student class """
+
+
+class Student():
+    """ Class to define a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {key: value for key, value \
+                in self.__dict__.items() if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for i, j in json.items():
+            self.__dict__[i] = j
