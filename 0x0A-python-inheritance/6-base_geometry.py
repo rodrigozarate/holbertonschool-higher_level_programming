9c10c4db586153c5440dd4e7a6478bfad08945b0
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-11 21:46:35 -0500

    [CREATE]

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module base Geometry """
+
+
+class BaseGeometry():
+    """ Base Geometry class """
+    def area(self):
+    """  area of geometry definition """
+        raise Exception("area() is not implemented")
