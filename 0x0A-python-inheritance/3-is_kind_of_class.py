8500018b997df50bd288bdd01bf822e0c28dc655
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-11 20:57:42 -0500

    [CREATE]

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module test for same kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Compare object to verfy if is instance of class """
+    return isinstance(obj, a_class)
