cf13ae185cdb895616170ec3e05a9991bdd7c11b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 14:35:14 -0500

    [UPDATE]

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ Add all arguments to a Python list """
+import sys
 
 
-save_to_json_file = __import__('').save_to_json_file
-load_from_json_file = __import('').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import('6-load_from_json_file').load_from_json_file
+
+open("add_item.json", "a")
+myfile = load_from_json_file("add_item.json")
+save_to_json_file(myfile + sys.argv[1:], "add_item.json")
