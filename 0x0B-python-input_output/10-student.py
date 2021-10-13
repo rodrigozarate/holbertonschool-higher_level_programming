6246f18d59f4423bfd694056eda5fb1b5b21caa0
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 23:53:41 -0500

    [FIX]

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,11 +5,13 @@
 class Student():
     """ Class to define a student """
     def __init__(self, first_name, last_name, age):
+        """ init of args for class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """ to json implementation """
         if type(attrs) is list and all([type(x) == str for x in attrs]):
             return {key: value for key, value \
                 in self.__dict__.items() if key in attrs}
