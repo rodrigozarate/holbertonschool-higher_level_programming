a3e03de2908efab0b84e8971d5a3448017c31f67
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 20:31:00 -0500

    [UPDATE]

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module represent pascal triangle """
+
+
+def pascal_triangle(n):
+    pascal = []
+    if n <= 0:
+        return pascal
+
+    i = 1
+    base = 1
+    while i <  n+1:
+        items = i + 1
+        """ pascal formula """
+        """ (x + y)^n """
+        list = []
+        coeficient = 1
+        for x in range(1, items):
+            list.append(coeficient)
+            coeficient = coeficient * (i - x) // x
+
+        pascal.append(list)
+        i += 1
+    return pascal
