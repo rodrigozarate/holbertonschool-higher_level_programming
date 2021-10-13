8d0f8dcc9797d3bdc0a082b7c80ad48d3719417a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 23:39:00 -0500

    [FIX]

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """ write to a file implementation """
     with open(filename, "w", encoding="utf-8") as myfile:
         myfile.write(text)
     return len(text)
