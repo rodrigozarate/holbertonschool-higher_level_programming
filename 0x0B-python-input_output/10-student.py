96356f1f24a42c5086fb20363c089e84e16c8bce
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 23:55:57 -0500

    [FIX] Avoid redunndant slash

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,6 +13,6 @@ class Student():
     def to_json(self, attrs=None):
         """ to json implementation """
         if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {key: value for key, value \
-                in self.__dict__.items() if key in attrs}
+            return {key: value for key, value
+                    in self.__dict__.items() if key in attrs}
         return self.__dict__
