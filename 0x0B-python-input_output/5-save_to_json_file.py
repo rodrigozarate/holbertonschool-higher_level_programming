0f2311038b0fab692a9d7a6ba868ab899763d4aa
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-12 23:47:35 -0500

    [FIX]

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,5 +4,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ save to json file implementation """
     with open(filename, "w", encoding="utf-8") as myfile:
         json.dump(my_obj, myfile)
