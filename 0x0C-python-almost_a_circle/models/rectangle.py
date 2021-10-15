89692f2354c364f53547880b226855a1a218f831
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-14 22:03:32 -0500

    [CREATE]

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Model rectangle """
+
+
+class Rectangle(Base):
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+
+def __init__(self, width, height, x=0, y=0, id=None)
+
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Model Square """
+
+
+class Square(Rectangle):
+def __init__(self, size, x=0, y=0, id=None):
+
