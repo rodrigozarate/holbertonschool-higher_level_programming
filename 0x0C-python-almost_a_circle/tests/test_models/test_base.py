16174ca6f070a7e95419a98acfc4fcf605d310b2
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-15 23:21:57 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,6 +8,6 @@ Base = base.Base
 
 class TestBase(unittest.TestCase):
     """ Unittest for Base Class """
-    def _set_id(self):
+    def test_set_id(self):
         b = Base(74)
         self.assertEqual(b.id, 74)
