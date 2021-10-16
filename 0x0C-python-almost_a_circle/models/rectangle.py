a0039d9e56293906c3bfc93c1f9e2b97383c9c3b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-16 11:34:57 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """ Model rectangle """
-
+from .base import Base
 
 class Rectangle(Base):
     """ Class Rectangle inherits from Base """
 
-    def __init__(self, width, height, x=0, y=0, id=None)
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
-        self.__x = 0
-        self.__y = 0
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        self._width = value
+        if not type(value) == int:
+            raise TypeError("width must be an integer")
+        if value <=0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -29,7 +34,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return self._x
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -37,7 +42,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        return self._y
+        return self.__y
 
     @y.setter
     def y(self, value):
