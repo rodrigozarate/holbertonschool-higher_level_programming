d2772d338997b672d158374d1fdcfc24b909bd91
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-16 00:05:50 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,10 +3,22 @@
 
 
 class Rectangle(Base):
+    """ Class Rectangle inherits from Base """
     __width -> width
     __height -> height
     __x -> x
     __y -> y
 
-def __init__(self, width, height, x=0, y=0, id=None)
+    def __init__(self, width, height, x=0, y=0, id=None)
+        self.__width = width
+        self.__height = height
+        self.__x = 0
+        self.__y = 0
 
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
