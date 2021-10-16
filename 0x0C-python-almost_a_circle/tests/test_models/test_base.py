6c10c39fa481206644a18dfe24b2da4c192f1e97
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-15 19:06:45 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,13 @@
->>> echo 4
+#!/usr/bin/python3
+""" Unittest for Base Class """
+
+
+import unittest
+from models import base
+Base = base.Base
+
+class TestBase(unittest.TestCase):
+    """ Unittest for Base Class """
+    def _set_id(self):
+        b = Base(74)
+        self.assertEqual(b.id, 74)
