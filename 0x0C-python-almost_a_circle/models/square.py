f88f3ac0ba9740bc9c448db2a4d1512e7fe26d42
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-16 22:34:35 -0500

    [UPDATE] Add size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,12 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".format(self.id,
                         self.x, self.y,
                         self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
