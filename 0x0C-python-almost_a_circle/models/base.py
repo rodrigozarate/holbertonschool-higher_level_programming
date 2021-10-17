df7c14c6b1fd495f7c59fe67f87f060c4f2efc28
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-17 17:16:58 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+        def to_json_string(list_dictionaries):
+            if not list_dictionaries:
+               return "[]"
+            return json.dumps(list_dictionaries)
