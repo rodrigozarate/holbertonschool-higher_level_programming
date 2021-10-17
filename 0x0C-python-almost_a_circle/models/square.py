bb22af7e2f5d161cd8a70599d752f11ad952970e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-16 22:11:49 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ Model Square """
+from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-def __init__(self, size, x=0, y=0, id=None):
+    """ Class Square """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
