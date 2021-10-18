1aa8223bb4e02aa83e498e112113806a397e3385
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-18 18:11:07 -0500

    [UPDATE]

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Test set for Square Class """
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """ Test cases for class Squaree """
+    def test_id_default(self):
+        s = Square(7, 4)
+        self.assertEqual(s.id, 1)
+
+    def test_size(self):
+        s_size = Square(7)
+        self.assertEqual(s_size.size, 49)
+
+    def test_width_height_x(self):
+        s_cero = Square(7, 4)
+        self.assertEqual(s_cero.zise, 7)
+        self.assertEqual(s_cero.x, 4)
+
+    def test_x_y_default(self):
+        s_one = Square(28, 7, 4)
+        self.assertEqual(s_one.x, 7)
+        self.assertEqual(s_one.y, 4)
+
+    def test_value_error_size(self):
+        with self.assertRaises(ValueError):
+            s_negative = Square(-7)
+        
+    def test_value_error_x(self):
+        with self.assertRaises(ValueError):
+            s_negative_x = Square(7, -4)
+
+    def test_type_error_string_size(self):
+        with self.assertRaises(TypeError):
+            s_string = Square("seven")
+
+    def test_type_error_string_x(self):
+        with self.assertRaises(TypeError):
+            s_string = Square(7, "four")
+
+    def test_type_error_list(self):
+        with self.assertRaises(TypeError):
+            s_list = Square([7, 4], 2, 8)
+
+    def test_area_success(self):
+        s_area = Square(7, 0, 0)
+        self.assertEqual(r_area.area(), 49)
+
+    def test_area_fail(self):
+        s_area = Square(7, 0, 0)
+        self.assertNotEqual(s_area.area(), 28)
