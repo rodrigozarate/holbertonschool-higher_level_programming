ad68de510d0af01c6d1592cfc630705511cf4daa
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-18 16:13:52 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,63 @@
 #!/usr/bin/python3
 """ Unittest for Base Class """
-
-
 import unittest
+import json
 from models import base
 Base = base.Base
 
+
 class TestBase(unittest.TestCase):
     """ Unittest for Base Class """
-    def test_set_id(self):
+
+    def test_set_id_none(self):
+        b_cero = Base()
+        self.assertEqual(b_cero.id, 2)
+
+    def test_set_id_none_fail(self):
+        b_one = Base()
+        self.assertNotEqual(b_one.id, 74)
+
+    def test_set_id_74(self):
         b = Base(74)
         self.assertEqual(b.id, 74)
+
+    def test_set_id_74_fail(self):
+        b74 = Base()
+        self.assertNotEqual(b74.id, 74)
+
+    def test_private(self):
+        b_private = Base(3)
+        with self.assertRaises(AttributeError):
+            print(b_private.nb_objects)
+        with self.assertRaises(AttributeError):
+            print(b_private.__nb_objects)
+
+    def test_to_json_string(self):
+        Base._Base__nb_objects = 0
+        b1 = {"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}
+        b2 = {"id": 5, "width": 4, "height": 3, "x": 2, "y": 1}
+        json_s = Base.to_json_string([b1, b2])
+        self.assertTrue(type(json_s) is str)
+        b0 = json.loads(json_s)
+        self.assertEqual(b0, [b1, b2])
+
+    def test_from_json_string_empty(self):
+        self.assertEqual([], Base.from_json_string(""))
+
+    def test_from_json_string_None(self):
+        self.assertEqual([], Base.from_json_string(None))
+
+    def test_from_json_string(self):
+        """Tests normal from_json_string"""
+        json_str = '[{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}, \
+                     {"id": 5, "width": 4, "height": 3, "x": 2, "y": 1}]'
+        json_l = Base.from_json_string(json_str)
+        self.assertTrue(type(json_l[0]) is dict)
+        self.assertTrue(type(json_l[1]) is dict)
+        self.assertTrue(type(json_l) is list)
+        self.assertEqual(len(json_l), 2)
+        self.assertEqual(json_l[0], {"id": 1, "width": 2, "height": 3, 
+                                     "x": 4, "y": 5})
+        self.assertEqual(json_l[1], {"id": 5, "width": 4, "height": 3,
+                                     "x": 2, "y": 1})
+
