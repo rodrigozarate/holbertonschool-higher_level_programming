fc3c9ecfb5d259e6eefbaef6c9d647f57017650f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-13 21:29:24 -0500

    [FIX]

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let givenArg = 'No argument'
 let argsCount = 0;
 process.argv.forEach((element, index) => {
   argsCount++;
 });
 if (argsCount <= 2) {
-  console.log(givenArg);
+  console.log('No argument');
 } else {
   console.log(process.argv[2]);
 }
