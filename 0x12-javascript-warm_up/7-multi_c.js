385bfe0ee2d24e279f46b18ddf5128c7eee670ce
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-13 22:35:17 -0500

    [FIX] missing parenthesis

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2]) {
-  for (let x = 0; x < parseInt(process.argv[2]; x++) {
+if (parseInt(process.argv[2])) {
+  for (let x = 0; x < parseInt(process.argv[2]); x++) {
     console.log('C is fun');
   }
 } else {
