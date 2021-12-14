f6e266854b63e81bdd57381c2e55267a4e07038f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-13 22:19:32 -0500

    [CREATE]

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (parseInt(process.argv[2]) {
+  for (let x = 0; x < parseInt(process.argv[2]; x++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of ocurrences');
+}
