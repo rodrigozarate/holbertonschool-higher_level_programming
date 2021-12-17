d60107637a23b1abdffd50b05e845304c1e789cf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-17 01:30:14 -0500

    [[CREATE]

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Simple list of table states filtered by initial N """
+
+import sys
+import MySQLdb
+
+
+def main():
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    holder = connection.cursor()
+    query = """SELECT citiesid, cities.name, states.name
+            FROM cities INNER JOIN states ON cities.state_id = states.id"""
+    holder.execute(query)
+    rows = holder.fetchall()
+    for row in rows:
+        print(row)
+    holder.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
