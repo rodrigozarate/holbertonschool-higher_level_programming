0caceffce25f1abc72104277ecbe0fdfe49f4c06
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-19 01:23:23 -0500

    [FIX]

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@
 import sys
 import MySQLdb
 
-def main():
+if __name__ == "__main__":
     connection = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,7 +22,3 @@ def main():
         print(row)
     holder.close()
     connection.close()
-
-
-if __name__ == "__main__":
-    main()
