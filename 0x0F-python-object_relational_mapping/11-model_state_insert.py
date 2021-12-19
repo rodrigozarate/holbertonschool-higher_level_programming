4dd69e9d220141eaba28cac02610bd4e78f6a2b5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-19 17:01:38 -0500

    [CREATE]

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" add object Louisiana """
+
+import sys
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+    session.close()
