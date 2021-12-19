668600f6010f0173445ed8808fa81a89cfee79cb
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-19 13:55:56 -0500

    [FIX]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        db=sys.argv[3]
         )
 
     holder = connection.cursor()
