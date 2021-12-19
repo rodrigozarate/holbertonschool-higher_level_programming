35c23c7c1d19b802ded7b0ed872148f0274a01d2
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-12-19 00:26:21 -0500

    [FIX]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     connection = MySQLdb.connect(
-        host="localhost",
+        host='localhost',
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
