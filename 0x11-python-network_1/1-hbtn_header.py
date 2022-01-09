9e8317b8cee537fd3b8c4b84824982985c4a7d91
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-08 21:49:39 -0500

    [CREATE]

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetch X-request-id """
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        content = response.getheader('X-Request-Id')
+
+    print(content)
