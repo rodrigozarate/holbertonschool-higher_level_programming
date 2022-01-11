ac34253d4146163dd3bc79594697ac5416889684
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-11 17:03:30 -0500

    [UPDATE]

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ Mod to find peak """
-""" verify start """
-""" verify end """
-""" define middle """
-""" Check previous """
-""" Check next """
 
+
+def find_peak(list_of_integers):
+    """ Get the peak by comparison """
+    elements = len(list_of_integers)
+    if list_of_integers = []:
+        return None
+    if elements == 1:
+        return list_of_integers[0]
+    if list_of_integers[0] >= list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[elements - 1] >= list_of_integers[elements - 2]:
+        return list_of_integers[elements - 1]
+    for i in range(1, elements - 1):
+        if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
+            return list_of_integers[i]
